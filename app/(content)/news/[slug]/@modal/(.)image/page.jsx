bd7335089185f0cc040b585178b57a76652a7863
vibo <vahab.afsharian94@gmail.com>
2025-09-20 13:01:18 +0300

    feat: Add group routes

--- app/(content)/news/[slug]/@modal/(.)image/page.jsx
+++ app/(content)/news/[slug]/@modal/(.)image/page.jsx
@@ -1,8 +1,12 @@
-import { notFound } from "next/navigation";
+"use client";
+
+import { notFound, useRouter } from "next/navigation";
 
 import { DUMMY_NEWS } from "@/dummy-news";
 
 export default function InterceptedImagePage({ params }) {
+  const rout = useRouter();
+
   const newsItemSlug = params.slug;
   const newsItem = DUMMY_NEWS.find(
     (newsItem) => newsItem.slug === newsItemSlug
@@ -14,11 +18,10 @@ export default function InterceptedImagePage({ params }) {
 
   return (
     <>
+      <div className="modal-backdrop" onClick={rout.refresh} />
       <dialog className="modal" open>
-        <div className="modal-backdrop">
-          <div className="fullscreen-image">
-            <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
-          </div>
+        <div className="fullscreen-image">
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
         </div>
       </dialog>
     </>
--- app/news/[slug]/layout.jsx
+++
@@ -1,3 +0,0 @@
-export default function NewsDetailLayout({ children }) {
-  return <>{children}</>;
-}
