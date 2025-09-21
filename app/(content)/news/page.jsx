69ae35b4716f7c391db1b365cdc8e6e4f42f361f
vibo <vahab.afsharian94@gmail.com>
2025-09-21 12:12:15 +0300

    feat: Implement news fetching and display in NewsPage component

--- app/(content)/news/page.jsx
+++ app/(content)/news/page.jsx
@@ -1,11 +1,32 @@
+"use client";
+
 import NewsList from "@/components/news-list";
-import { DUMMY_NEWS } from "@/dummy-news";
+import { useEffect, useState } from "react";
 
 export default function NewsPage() {
+  const [news, setNews] = useState();
+
+  useEffect(() => {
+    async function fetchNews() {
+      const respone = await fetch("http://localhost:8080/news");
+      const news = await respone.json();
+
+      setNews(news);
+    }
+
+    fetchNews();
+  }, []);
+
+  let newsContent;
+
+  if (news) {
+    return <NewsList news={news} />;
+  }
+
   return (
     <>
       <h1>This is news page</h1>
-      <NewsList news={DUMMY_NEWS} />
+      {newsContent}
     </>
   );
 }
